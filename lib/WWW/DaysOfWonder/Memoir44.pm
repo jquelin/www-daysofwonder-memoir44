02b29883ea1bfab3c6bb5f2e8916364eb44ea078
Jérôme Quelin <jquelin@gmail.com>
2010-01-02 14:56:26 +0100

    placeholder for main module

---
+++ lib/WWW/DaysOfWonder/Memoir44.pm
@@ -0,0 +1,61 @@
+use 5.010;
+use strict;
+use warnings;
+
+package WWW::DaysOfWonder::Memoir44;
+# ABSTRACT: download & manage memoir44 scenarios from its website
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+Memoir'44 is a unique historical game where players command a horde of
+little plastic Army men facing-off in dozens of WWII battles on an
+oversize hex game board. Each battle scenario mimics the historical
+terrain, troop placements and objectives of each army. Deploying forces
+through a variety of Command cards, the smart commander uses the unique
+skills of his units - infantry, paratroopers, tanks, artillery,
+commandos and resistance fighters - to its greatest strength.
+
+The game comes with some bundled scenarios, but there are a lot more
+available online. This module allows one to list and retrieve those
+officials and non-officials scenarios.
+
+Don't use this module directly, but check the L<mem44> command for an
+easy to use way of fetching the scenarios.
+
+
+
+=head1 SEE ALSO
+
+You can find more information on memoir44 on its website:
+L<http://www.daysofwonder.com/memoir44/>
+
+You can find more information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/WWW-DaysOfWonder-Memoir44>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-DaysOfWonder-Memoir44>
+
+=item * Git repository
+
+L<http://github.com/jquelin/www-daysofwonder-memoir44.git>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/WWW-DaysOfWonder-Memoir44>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/WWW-DaysOfWonder-Memoir44>
+
+=back
+
+
