2a139e1cdeaf12a9288d7533f521b8280cf5bb78
Jérôme Quelin <jquelin@gmail.com>
2010-01-02 15:43:03 +0100

    initial db schema

---
+++ lib/WWW/DaysOfWonder/Memoir44/DB.pm
@@ -0,0 +1,30 @@
+use 5.010;
+use strict;
+use warnings;
+
+package WWW::DaysOfWonder::Memoir44::DB;
+# ABSTRACT: orlite for mem44 sqlite db
+
+use File::Spec::Functions qw{ catdir };
+
+use WWW::DaysOfWonder::Memoir44::Utils qw{ get_db_file get_dist_dir };
+
+use ORLite ();
+use ORLite::Migrate {
+    create        => 1,
+    file          => get_db_file(),
+    user_revision => 1,
+    timeline      => catdir( get_dist_dir(), 'db-timeline' ),
+}; #, '-DEBUG'; # uncomment the trailing -DEBUG for debug infos on orlite magic
+
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+This module is used to fetch auto-magically the database schema from the
+real sqlite database and map the tables to sub-modules, thanks to
+L<ORLite>. It will also create the database if it doesn't exist, with
+L<ORLite::Migrate>.
+
