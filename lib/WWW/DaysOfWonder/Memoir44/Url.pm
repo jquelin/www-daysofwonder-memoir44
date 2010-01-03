d541510cfad431c96e4322d98212596ea0c5f58d
Jérôme Quelin <jquelin@gmail.com>
2010-01-03 12:08:05 +0100

    introducing dow url encapsulation

---
+++ lib/WWW/DaysOfWonder/Memoir44/Url.pm
@@ -0,0 +1,93 @@
+use 5.010;
+use strict;
+use warnings;
+
+package WWW::DaysOfWonder::Memoir44::Url;
+# ABSTRACT: encapsulation of days of wonder urls
+
+use Moose;
+use MooseX::Has::Sugar;
+use URI;
+
+use overload '""' => \&as_string;
+
+
+# -- attributes
+
+=attr $url->source;
+
+The scenarios source. See the C<Source> type in
+L<WWW::DaysOfWonder::Memoir44::Types>.
+
+=cut
+
+has source => ( ro, isa=>'Str', required   );
+has _uri   => ( ro, isa=>'URI', lazy_build );
+
+
+# -- initializers & builders
+
+sub _build__uri {
+    my $self = shift;
+    my $uri  = URI->new;
+    $uri->scheme( 'http' );
+    $uri->host( 'www.daysofwonder.com' );
+    $uri->path( '/memoir44/fr/scenario_list/' );
+
+    my %options = (
+        sellang    => 'fr',
+        start      => 0,
+        page_limit => 2000,
+        status     => $self->source,
+    );
+
+    $uri->query_form( \%options );
+    return $uri;
+}
+
+
+# -- public methods
+
+=method my $str = $url->as_string;
+
+Stringifies the object in a well-formed url. This is the method called
+when the object needs to be stringified by perl due to the context.
+
+=cut
+
+sub as_string {
+    my $self = shift;
+    return $self->_uri->as_string;
+}
+
+# canonical url:
+# http://www.daysofwonder.com/memoir44/fr/scenario_list/?sellang=fr&start=0&page_limit=2000
+# other valid http options:
+#   status      game = shipped, approved = official, public = non-dow
+#   selpack_tp  terrain pack, 
+#   selpack_ef  east front
+#   selpack_pt  pacific theater
+#   selpack_ap  air pack
+#   selpack_mt  mediterranean theater
+#   selpack_bm  battle map
+#   selpack_cb  carnets campagne
+# with values: 0 = undef, 1 = with, 2 = without
+# eg: selpack_tp=1&selpack_ef=2
+
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+    use WWW::DaysOfWonder::Memoir44::Url;
+    my $url = WWW::DaysOfWonder::Memoir44::Url->new( { source => 'game' } );
+    print $url;
+
+
+=head1 DESCRIPTION
+
+This modules encapsulates urls to fetch scenarios from Days of Wonder.
+Depending on various criterias (cf attributes), the url listing the
+available scenarios will be different.
+
