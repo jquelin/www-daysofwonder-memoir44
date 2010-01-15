3763c83181a537e1464c6f276ed0f14da1746855
Jérôme Quelin <jquelin@gmail.com>
2010-01-15 14:34:21 +0100

    base class for subcommands

---
+++ lib/WWW/DaysOfWonder/Memoir44/App/Command.pm
@@ -0,0 +1,27 @@
+use 5.010;
+use strict;
+use warnings;
+
+package WWW::DaysOfWonder::Memoir44::App::Command;
+# ABSTRACT: base class for sub-commands
+
+use App::Cmd::Setup -command;
+
+
+# -- public methods
+
+
+1;
+__END__
+
+=for Pod::Coverage::TrustPod
+    description
+    opt_spec
+    execute
+
+
+=head1 DESCRIPTION
+
+This module is the base class for all sub-commands. It doesn't do
+anything special currently but trusting methods for pod coverage.
+
