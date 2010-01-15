59095bbdc2c905e9016f18e4641d114ea0ff957e
Jérôme Quelin <jquelin@gmail.com>
2010-01-15 13:01:01 +0100

    new command list (currently empty)

---
+++ lib/WWW/DaysOfWonder/Memoir44/App/Command/list.pm
@@ -0,0 +1,43 @@
+use 5.010;
+use strict;
+use warnings;
+
+package WWW::DaysOfWonder::Memoir44::App::Command::list;
+# ABSTRACT: list scenarios according to various criterias
+
+use WWW::DaysOfWonder::Memoir44::App -command;
+use WWW::DaysOfWonder::Memoir44::DB;
+
+
+# -- public methods
+
+sub description {
+'List the scenarios available in the database according to various
+criterias. The database must exist - see the update command for
+this action.';
+}
+
+sub opt_spec {
+    my $self = shift;
+    return;
+}
+
+sub execute {
+    my $self = shift;
+    say "list";
+}
+
+
+1;
+__END__
+
+=for Pod::Coverage::TrustPod
+    description
+    opt_spec
+    execute
+
+=head1 DESCRIPTION
+
+This command list the scenarios available in the database, according to
+various criterias. The database must exist - see the update command for
+this action.
