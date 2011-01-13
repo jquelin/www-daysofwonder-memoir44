2a1d347440203af049fde0e994f68389542c5740
Jérôme Quelin <jquelin@gmail.com>
2011-01-13 15:31:36 +0100

    adding a scenarios db

---
+++ lib/WWW/DaysOfWonder/Memoir44/DB/Scenarios.pm
@@ -0,0 +1,63 @@
+use 5.012;
+use strict;
+use warnings;
+
+package WWW::DaysOfWonder::Memoir44::DB::Scenarios;
+# ABSTRACT: scenarios database
+
+use JSON;
+use MooseX::Singleton;
+use MooseX::Has::Sugar;
+use MooseX::Storage;
+use Path::Class;
+
+use WWW::DaysOfWonder::Memoir44::Utils;
+
+
+with Storage( format => 'JSON', io => 'File' );
+
+my $dbfile = DATADIR->file( "scenarios.json" );
+
+has scenarios => (
+    traits     => ['Array'],
+    is         => 'ro',
+    isa        => 'ArrayRef[WWW::DaysOfWonder::Memoir44::Scenario]',
+    default    => sub { [] },
+    auto_deref => 1,
+    handles    => {
+        nb_scenarios => 'count',     # my $nb = $db->nb_scenarios;
+        add          => 'push',      # $db->add( $scenario, $scenario );
+    }
+);
+
+
+ # -- public methods
+  
+=method add
+    
+    my $db = WWW::DaysOfWonder::Memoir44::DB::Scenarios->instance;
+    $db->add( @scenarios );
+
+Store a new scenario in the scenarios database.
+
+=cut
+
+# implemented by the 'Array' trait of the 'scenarios' attribute.
+
+
+=method write
+
+    my $db = WWW::DaysOfWonder::Memoir44::DB::Scenarios->instance;
+    $db->write;
+
+Store the whole scenarios database to a file.
+
+=cut
+
+sub write {
+    my $self = shift;
+    $self->store( $dbfile->stringify );
+}
+
+1;
+__END__
